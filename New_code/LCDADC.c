a1a8f317f9777992245bf47b7ff7dbec5f57bb9f
Bryan Luu <bryanluu30794@gmail.com>
2015-02-18 16:52:36 -0800

    basic Servo Driver code
    Use pins 1.2 and 1.3 as Open-Drain

--- New_code/LCDADC.c
+++ New_code/LCDADC.c
@@ -20,154 +20,9 @@
 #define LCD_D0 P2_4
 #define CHARS_PER_LINE 16
 
-// ==================MAIN FUNCTION===========================
-void main (void)
-{
-	Setup();
-	
-		
-	// LOOP	
-	while(1)
-	{
-		UpdateString();
-		//P3_0=0;
-		//Wait1S();
-		//P3_0=1;
-		//Wait1S();
-	}
-	
-}
-
-void Setup(void)
-{
-	InitPorts();
-	LCD_8BIT();
-	
-	InitSerialPort();
-	InitADC();
-}
-
-
-void UpdateString(void)
-{
-	char string1[17];
-	char string2[17];
-	sprintf(string1, "1: %i, 2: %i", AD1DAT0, AD1DAT1);
-	sprintf(string2, "3: %i, 4: %i", AD1DAT2, AD1DAT3);
-	LCDprint(string1,1,1);
-	LCDprint(string2,2,1);
-}
-
-
-// ================= Initialization Funcs ===================
-
-void InitPorts(void)
-{
-	P0M1=0;
-	P0M2=0;
-	P1M1=0;
-	P1M2=0;
-	P2M1=0;
-	P2M2=0;
-	P3M1=0;
-	P3M2=0;
-}
-
-void InitSerialPort(void)
-{
-	BRGCON=0x00; //Make sure the baud rate generator is off
-	BRGR1=((XTAL/BAUD)-16)/0x100;
-	BRGR0=((XTAL/BAUD)-16)%0x100;
-	BRGCON=0x03; //Turn-on the baud rate generator
-	SCON=0x52; //Serial port in mode 1, ren, txrdy, rxempty
-	P1M1=0x00; //Enable pins RxD and Txd
-	P1M2=0x00; //Enable pins RxD and Txd
-}
-
-void InitADC(void)
-{
-	// Set adc1 channel pins as input only 
-	P0M1 |= (P0M1_4 | P0M1_3 | P0M1_2 | P0M1_1);
-	P0M2 &= ~(P0M1_4 | P0M1_3 | P0M1_2 | P0M1_1);
-
-	BURST1=1; //Autoscan continuos conversion mode
-	ADMODB = CLK0; //ADC1 clock is 7.3728MHz/2
-	ADINS  = (ADI13|ADI12|ADI11|ADI10); // Select the four channels for conversion
-	ADCON1 = (ENADC1|ADCS10); //Enable the converter and start immediately
-	while((ADCI1&ADCON1)==0); //Wait for first conversion to complete
-}
-
-
-
-
-// =============== LCD Funcs =============
-
-void LCD_8BIT (void)
-{
-	LCD_E=0;  // Resting state of LCD's enable is zero
-	LCD_RW=0; // We are only writing to the LCD in this program
-	waitms(20);
-	// First make sure the LCD is in 8-bit mode
-	WriteCommand(0x33);
-	WriteCommand(0x33);
-	WriteCommand(0x33); // Stay in 8-bit mode
-
-	// Configure the LCD
-	WriteCommand(0x38);
-	WriteCommand(0x0c);
-	WriteCommand(0x01); // Clear screen command (takes some time)
-	waitms(20); // Wait for clear screen command to finsih.
-}
-
-void LCDprint(char * string, unsigned char line, bit clear)
-{
-	unsigned char j;
-
-	WriteCommand(line==2?0xc0:0x80);
-	waitms(5);
-	for(j=0; string[j]!=0; j++)	WriteData(string[j]);// Write the message
-	if(clear) for(; j<CHARS_PER_LINE; j++) WriteData(' '); // Clear the rest of the line
-}
-
-
-void LCD_pulse (void)
-{
-	LCD_E=1;
-	Wait50us();
-	LCD_E=0;
-}
-
-
-void LCD_byte (unsigned char x)
-{
-	// The accumulator in the C8051Fxxx is bit addressable!
-	ACC=x;
-	LCD_D7=ACC_7;
-	LCD_D6=ACC_6;
-	LCD_D5=ACC_5;
-	LCD_D4=ACC_4;
-	LCD_D3=ACC_3;
-	LCD_D2=ACC_2;
-	LCD_D1=ACC_1;
-	LCD_D0=ACC_0;
-	LCD_pulse();
-}
-
-
-void WriteData (unsigned char x)
-{
-	LCD_RS=1;
-	LCD_byte(x);
-	waitms(2);
-}
-
-void WriteCommand (unsigned char x)
-{
-	LCD_RS=0;
-	LCD_byte(x);
-	waitms(5);
-}
-
+/*
+		SCROLL TO BOTTOM FOR MAIN FUNCTIONS
+*/
 
 // ======= Delay Funcs =======
 
@@ -200,6 +55,170 @@ L1: djnz R0, L1 ; 2 machine cycles-> 2*0.27126us*184=100us
     djnz R2, L3 ; 0.025s*40=1s
     _endasm;
 }
+
+// =============== LCD Funcs =============
+
+void LCD_pulse (void)
+{
+	LCD_E=1;
+	Wait50us();
+	LCD_E=0;
+}
+
+void LCD_byte (unsigned char x)
+{
+	// The accumulator in the C8051Fxxx is bit addressable!
+	ACC=x;
+	LCD_D7=ACC_7;
+	LCD_D6=ACC_6;
+	LCD_D5=ACC_5;
+	LCD_D4=ACC_4;
+	LCD_D3=ACC_3;
+	LCD_D2=ACC_2;
+	LCD_D1=ACC_1;
+	LCD_D0=ACC_0;
+	LCD_pulse();
+}
+
+void WriteData (unsigned char x)
+{
+	LCD_RS=1;
+	LCD_byte(x);
+	waitms(2);
+}
+
+void WriteCommand (unsigned char x)
+{
+	LCD_RS=0;
+	LCD_byte(x);
+	waitms(5);
+}
+
+void LCD_8BIT (void)
+{
+	LCD_E=0;  // Resting state of LCD's enable is zero
+	LCD_RW=0; // We are only writing to the LCD in this program
+	waitms(20);
+	// First make sure the LCD is in 8-bit mode
+	WriteCommand(0x33);
+	WriteCommand(0x33);
+	WriteCommand(0x33); // Stay in 8-bit mode
+
+	// Configure the LCD
+	WriteCommand(0x38);
+	WriteCommand(0x0c);
+	WriteCommand(0x01); // Clear screen command (takes some time)
+	waitms(20); // Wait for clear screen command to finsih.
+}
+
+void LCDprint(char * string, unsigned char line, bit clear)
+{
+	unsigned char j;
+
+	WriteCommand(line==2?0xc0:0x80);
+	waitms(5);
+	for(j=0; string[j]!=0; j++)	WriteData(string[j]);// Write the message
+	if(clear) for(; j<CHARS_PER_LINE; j++) WriteData(' '); // Clear the rest of the line
+}
+
+
+// ================= Initialization Funcs ===================
+
+void InitMotorPorts(void)
+{
+	/*
+	P1M1.2=1; //Set pin 1.2 to be Open Drain
+	P1M2.2=1;
+	P1M1.3=1; //Set pin 1.3 to be Open Drain
+	P1M2.3=1;*/
+}
+
+void InitPorts(void)
+{
+	P0M1=0;
+	P0M2=0;
+	P1M1=0;
+	P1M2=0;
+	P2M1=0;
+	P2M2=0;
+	InitMotorPorts();
+}
+
+
+void InitSerialPort(void)
+{
+	BRGCON=0x00; //Make sure the baud rate generator is off
+	BRGR1=((XTAL/BAUD)-16)/0x100;
+	BRGR0=((XTAL/BAUD)-16)%0x100;
+	BRGCON=0x03; //Turn-on the baud rate generator
+	SCON=0x52; //Serial port in mode 1, ren, txrdy, rxempty
+	P1M1=0x00; //Enable pins RxD and Txd
+	P1M2=0x00; //Enable pins RxD and Txd
+}
+
+void InitADC(void)
+{
+	// Set adc1 channel pins as input only 
+	P0M1 |= (P0M1_4 | P0M1_3 | P0M1_2 | P0M1_1);
+	P0M2 &= ~(P0M1_4 | P0M1_3 | P0M1_2 | P0M1_1);
+
+	BURST1=1; //Autoscan continuos conversion mode
+	ADMODB = CLK0; //ADC1 clock is 7.3728MHz/2
+	ADINS  = (ADI13|ADI12|ADI11|ADI10); // Select the four channels for conversion
+	ADCON1 = (ENADC1|ADCS10); //Enable the converter and start immediately
+	while((ADCI1&ADCON1)==0); //Wait for first conversion to complete
+}
+
+
+
+
+// ==================MAIN FUNCTION===========================
+
+void Setup(void)
+{
+	InitPorts();
+	LCD_8BIT();
+	
+	InitSerialPort();
+	InitADC();
+}
+
+
+void UpdateString(void)
+{
+	char string1[17];
+	char string2[17];
+	sprintf(string1, "1: %i, 2: %i", AD1DAT0, AD1DAT1);
+	sprintf(string2, "3: %i, 4: %i", AD1DAT2, AD1DAT3);
+	LCDprint(string1,1,1);
+	LCDprint(string2,2,1);
+}
+
+void main (void)
+{
+	Setup();
+	
+		
+	// LOOP	
+	while(1)
+	{
+		UpdateString();
+		P1_2=1; //Drive pin 1.2 to 5V (by OpenDrain)
+		Wait1S();
+		P1_2=0; //Drive pin 1.2 to LOW
+		Wait1S();
+	}
+	
+}
+
+
+
+
+
+
+
+
+
 
 
 
